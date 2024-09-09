6cbf63bb52076627a44b52a5e96a1b713efad078
wendellsantos2 <wemdellcos@gmail.com>
2024-09-09 10:25:53 -0300

    feat : update code

---
+++ SistemaFinceiro/Infra/Repositorio/Generics/InterfaceGenerics.cs
@@ -0,0 +1,15 @@
+﻿namespace Infra.Repositorio.Generics
+{
+    public class InterfaceGenerics<T> where T : class
+    {
+        public interface InterfaceGeneric<T> where T : class
+        {
+            Task Add(T Objeto);
+            Task Update(T Objeto);
+            Task Delete(T Objeto);
+            Task<T> GetEntityById(int Id);
+            Task<List<T>> List();
+
+        }
+    }
+}
